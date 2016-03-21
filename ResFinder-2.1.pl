5ee39126f92a39b1ff9cb7069f880e6c8c40a4bd
Martin Christen Frølund Thomsen <mcft@cbs.dtu.dk>
2016-03-21 12:43:47 +0100

    cleanup

--- ResFinder-2.1.pl
+++ ResFinder-2.1.pl
@@ -999,7 +999,7 @@ foreach my $key (sort keys %GENE_RESULTS_HASH2) {
   $alignment .= "\n--------------------------------------------------------------------------------\n\n";
 }#end foreach
   
-		#WRITING standard_output.txt
+		#WRITING results.txt
 open (TXTRESULTS, '>>',"$dir/results.txt") or die("Error! Could not write to $dir"."results.txt\n");
 print TXTRESULTS $txtresults;
 print TXTRESULTS $contigtable;
