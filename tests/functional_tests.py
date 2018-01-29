c6f5cfad4c5d3371f67d0e78a3fec4d3af65605b
RolfKaas <rolkaas@gmail.com>
2018-01-29 14:05:19 +0100

    Add acquired resfinder test

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,8 +1,16 @@
 import unittest
-from subprocess import Popen, PIPE, STDOUT, run
+from subprocess import PIPE, run
+import os
 
 
 class ResFinderRunTest(unittest.TestCase):
+    test_data = {
+        "test1": "data/test_isolate_01.fa",  # Test published resistance
+        "test3": "data/test_isolate_03.fa",  # Test no resistance
+    }
+
+    def setUp(self):
+        os.makedirs("running_test", exist_ok=True)
 
     def test_on_data_with_just_acquired_resgene(self):
         # Maria has an E. coli isolate, with unknown resistance.
@@ -15,6 +23,12 @@ class ResFinderRunTest(unittest.TestCase):
                     check=True)
         output = procs.stdout.decode()
         self.assertIn("--help", output)
+
+        # Maria goes on to run ResFinder for acquired genes with her E. coli
+        # isolate
+#        cmd_acquired = ("python3 ../run_resfinder.py"
+#                        " -i " + test_data["test1"]
+#                        " ")
 
 
 if __name__ == "__main__":
