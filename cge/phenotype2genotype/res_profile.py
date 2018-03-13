c0d91ee40b9989b8ec490fd81f04d02ffc2eab6b
RolfKaas <rolkaas@gmail.com>
2018-03-13 16:49:46 +0100

    debug print

--- cge/phenotype2genotype/res_profile.py
+++ cge/phenotype2genotype/res_profile.py
@@ -48,8 +48,9 @@ class PhenoDB(dict):
             for line in fh:
                 try:
                     line_counter += 1
+                    eprint("Line " + str(line_counter))
                     # line = line.encode("latin_1")
-                    # line = line.rstrip()
+                    line = line.rstrip()
                     line_list = line.split("\t")
                     line_list = list(map(str.rstrip, line_list))
 
