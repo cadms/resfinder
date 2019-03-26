ac079346680dabc4bfcd7436e23f945366587c30
RolfKaas <rolkaas@gmail.com>
2019-03-26 09:37:30 +0100

    Add feature to allow overlap of resistance genes

--- resfinder.py
+++ resfinder.py
@@ -66,12 +66,13 @@ class ResFinder(CGEFinder):
          fh.write(result_str[4])
 
    def blast(self, inputfile, out_path, min_cov=0.9, threshold=0.6,
-             blast="blastn"):
+             blast="blastn", allowed_overlap=0):
       """
       """
       blast_run = Blaster(inputfile=inputfile, databases=self.databases,
                           db_path=self.db_path, out_path=out_path,
-                          min_cov=min_cov, threshold=threshold, blast=blast)
+                          min_cov=min_cov, threshold=threshold, blast=blast,
+                          allowed_overlap=allowed_overlap)
       self.blast_results = blast_run.results
       return blast_run
 
@@ -464,6 +465,11 @@ if __name__ == '__main__':
                        dest="threshold",
                        help="Blast threshold for identity",
                        default=0.90)
+   parser.add_argument("-ao", "--acq_overlap",
+                       help="Genes are allowed to overlap this number of\
+                             nucleotides. Default: 30.",
+                       type=int,
+                       default=30)
    args = parser.parse_args()
 
    ##########################################################################
@@ -539,7 +545,8 @@ if __name__ == '__main__':
 
       blast_run = finder.blast(inputfile=inputfile, out_path=out_path,
                                min_cov=min_cov, threshold=threshold,
-                               blast=args.blast_path)
+                               blast=args.blast_path,
+                               allowed_overlap=args.acq_overlap)
 
       finder.write_results(out_path=out_path, result=blast_run,
                            res_type=ResFinder.TYPE_BLAST)
