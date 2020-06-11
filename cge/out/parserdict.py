5cec3378f81a3363acd506d0de2c07eb191093ea
RolfKaas <rolkaas@gmail.com>
2020-06-11 11:47:29 +0200

    Add custom parser feature

--- cge/out/parserdict.py
+++ cge/out/parserdict.py
@@ -8,18 +8,21 @@ class ParserDict(dict):
     def __init__(self, input_parser=None):
 
         if(input_parser is None):
-            val_parser_list = self.get_method_names(ValueParsers)
+            parser_class = ValueParsers
         else:
-            val_parser_list = self.get_method_names(input_parser)
+            parser_class = input_parser.__class__
+
+        val_parser_list = self.get_method_names(parser_class)
 
         for parser in val_parser_list:
             if(parser.startswith("parse_")):
                 parse_key = parser[6:]
-                self[parse_key] = getattr(ValueParsers, parser)
+                self[parse_key] = getattr(parser_class, parser)
             else:
-                raise SyntaxError(("A function in the ValueParsers class did "
+                raise SyntaxError(("A function in the {} class did "
                                    "not start with 'parse_'. Function is "
-                                   "named: {}".format(parser)))
+                                   "named: {}"
+                                   .format(parser_class.__name__, parser)))
 
     @staticmethod
     def get_method_names(cls):
